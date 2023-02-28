9cf4b45f3becf351bcd122699411889eb33df91f
dknapp67 <douglas.knapp@outlook.com>
2023-02-27 20:14:56 -0600

    Added includes.js

---
+++ includes.js
@@ -0,0 +1,7 @@
+let str = 'To be, or not to be, that is the question.';
+
+console.log(str.includes('To be'));
+console.log(str.includes('question'));
+console.log(str.includes('nonexistent'));
+console.log(str.includes('To be', 1));
+console.log(str.includes('TO BE'));
