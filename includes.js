0e83de01137901395379b53cfada63cf754e2f28
dknapp67 <douglas.knapp@outlook.com>
2023-02-27 20:51:20 -0600

    Eddited includes.js

--- includes.js
+++ includes.js
@@ -1,7 +1,8 @@
-let str = 'To be, or not to be, that is the question.';
+let soliloquy = 'To be, or not to be, that is the question.';
 
-console.log(str.includes('To be'));
-console.log(str.includes('question'));
-console.log(str.includes('nonexistent'));
-console.log(str.includes('To be', 1));
-console.log(str.includes('TO BE'));
+soliloquy.includes('To be');        // Does it include the string "To be"?
+soliloquy.includes('question');     // What about "question"?
+soliloquy.includes('nonexistent');  // This string doesn't appear
+soliloquy.includes('TO BE');        // String inclusion is case-sensitive
+soliloquy.includes('To be', 1);     // Can you guess what this one means?
+soliloquy.includes("o be,", 1);     // A hint for the previous one
